e2f5d979d14faa453ffef840ee1791050490cdad
Romaric Drigon <romaric.drigon@gmail.com>
2011-11-27 00:00:08 +0100

    Bug fixed
    Prevents multiples instances of strobe to be launched

---
+++ README.txt
@@ -0,0 +1,5 @@
+Stroboscope.js is a Javascript stroboscope.
+
+It's basically an animated html page.
+
+You can tune frequency, synchronize the beat (with UTC time, if you use it accross several computers), change the colors...
--- stroboscope.js
+++ stroboscope.js
@@ -5,6 +5,8 @@
 	strobo.blink = 0;
 	strobo.timer = null;
 	strobo.frequency = 1;
+	strobo.clock = null;
+	strobo.running = false;
 
 /*
  * Set the config area
@@ -20,22 +22,30 @@ function setConfig() {
 				}
 	});
 	
+	// launch (or stop) animation button
 	$('button', '#launch').button(); // button withing launch div
-	$('#launch').click(launchStrobo);
+	$('#launch').click(startStrobo);
+	
+	// synchronize button
+	$('button', '#sync').button();
+	$('#sync').click(syncStrobo);
 }
 
-function launchStrobo(skipButton) {
+/*
+ * Pretty straight-forward functions about the stroboscope
+ */
+function startStrobo() {
 	blink = 0;
 	
-	if (skipButton == true)
-	{
-		// set stop button
-		$('#launch').unbind('click'); // destroy previous handler
-		$('#launch').click(stopStrobo);
-		$('#launch').html('<button>Stop animation</button>');
-		$('button', '#launch').button();
-	}
+	// set stop button
+	$('#launch').unbind('click'); // destroy previous handler
+	$('#launch').click(stopStrobo);
+	$('#launch').html('<button>Stop animation</button>');
+	$('button', '#launch').button();
 	
+	launchStrobo();
+}
+function launchStrobo() {	
 	$('body').each(function() {
 	    strobo.timer = setInterval(function() {
 	        if (strobo.blink === 0) {
@@ -46,25 +56,48 @@ function launchStrobo(skipButton) {
 	            strobo.blink = 0;
 	        }
 		}, 60000/strobo.frequency);
-	})
-}
-
-function stopStrobo(skipButton) {
-	$('body').each(function() {
-		clearInterval(strobo.timer);
-	})
+	});
 	
-	if (skipButton == true)
+	strobo.running = true;
+}
+function stopStrobo(skipButton) {
+	if (strobo.running = true) {
+		$('body').each(function() {
+			clearInterval(strobo.timer);
+		});
+		
+		strobo.running = false;
+	}
+	
+	if (skipButton != true)
 	{
 		// reset start button
 		$('#launch').unbind('click'); // destroy previous handler
-		$('#launch').click(launchStrobo);
+		$('#launch').click(startStrobo);
 		$('#launch').html('<button>Launch animation</button>');
 		$('button', '#launch').button();
 	}
 }
-
 function modifyStrobo() {
-	stopStrobo(true);
-	launchStrobo(true);
+	if (strobo.running == true) { // relaunch only if it was running
+		stopStrobo(true);
+		launchStrobo();
+	}
+}
+
+/*
+ * Let's the fun begin: will sync the stroboscope using time
+ */
+function syncStrobo() {
+	strobo.clock = window.setInterval(checkTime, 1000);
+}
+function checkTime() {
+	 var currentTime = new Date ();
+	 var currentSeconds = currentTime.getSeconds ();
+	 
+	 if (currentSeconds === 0)
+	 {
+	 	modifyStrobo();
+	 	window.clearInterval(strobo.clock);
+	 }
 }
