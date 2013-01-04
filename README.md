0a3fbd32212273aae7502c5e8c99a033e2fd2d2e
Romaric Drigon <romaric.drigon@gmail.com>
2013-01-04 13:51:22 +0100

    Working! News features!

--- README.md
+++ README.md
@@ -2,11 +2,13 @@
 
 Stroboscope.js is a Javascript stroboscope.
 
-It's basically an animated html page.
+In manual mode, you can tune frequency, synchronize the beat (with UTC time, if you use it accross several computers), change the colors...
 
-You can tune frequency, synchronize the beat (with UTC time, if you use it accross several computers), change the colors...
+With Google Chrome browser, you can access the microphone (using the experimental Web AudioAPI) and sync with music!
+
+Settings are saved to localStorage when you quit the application.
 
 ## (non) license
 
 Feel free to re-use, modify it.
-It uses jQuery, jQuery UI and jPicker plugin which have their own licenses.
+It uses jQuery, jQuery UI, jPicker plugin, and [dancer.js](https://github.com/jsantell/dancer.js) which have their own licenses.
--- stroboscope.js
+++ stroboscope.js
@@ -13,15 +13,110 @@
 	strobo.timer = null;
 	strobo.frequency = 1;
 	strobo.running = false;
+    strobo.dancer = new Dancer();
+    strobo.kick_config = {
+        threshold: 0.001,
+        frequency: [0,10],
+        decay: 0.0003,
+        onKick: function (mag) {
+            $('body').css('background', '#'+$.jPicker.List[1].color.active.val('hex'));
+            strobo.blink = 1;
+        },
+        offKick: function (mag) {
+            $('body').css('background', '#'+$.jPicker.List[0].color.active.val('hex'));
+            strobo.blink = 0;
+        }
+    };
+    strobo.kick = strobo.dancer.createKick(strobo.kick_config);
 
 /*
  * Set the config area
  */
-function setConfig() {	
+function setConfig() {
+    /*
+        Common controls
+     */
+
+    $('#picker1').jPicker({
+        window:
+        {
+            expandable: true,
+            position:
+            {
+                x: 'right',
+                y: 'bottom'
+            }
+        },
+        color:
+        {
+            active: new $.jPicker.Color({hex: '111111'})
+        },
+        images:
+        {
+            clientPath: 'jpicker/images/'
+        }
+
+    }, modifyStrobo); // don't forget the callback
+    $('#picker2').jPicker({
+        window:
+        {
+            expandable: true,
+            position:
+            {
+                x: 'right',
+                y: 'bottom'
+            }
+        },
+        color:
+        {
+            active: new $.jPicker.Color({hex: 'ffffff'})
+        },
+        images:
+        {
+            clientPath: 'jpicker/images/'
+        }
+
+    }, modifyStrobo);
+
+    // save values when user leaves the page
+    window.onbeforeunload = function() {
+        // we save editor content to localStorage
+        if (localStorage) {
+            localStorage['frequency'] = strobo.frequency;
+            localStorage['kick_config'] = JSON.stringify(strobo.kick_config);
+            localStorage['picker0'] = $.jPicker.List[0].color.active.val('hex');
+            localStorage['picker1'] = $.jPicker.List[1].color.active.val('hex');
+
+            return "Do you really want to end the party?\nYour settings were saved to localStorage";
+        }
+
+        return "Do you really want to end the party?\nYou will lost your settings!";
+    };
+    // and load saved settings
+    if (localStorage) {
+        if (localStorage['frequency']) {
+            strobo.frequency = localStorage['frequency'];
+            strobo.kick_config = JSON.parse(localStorage['kick_config']);
+            $.jPicker.List[0].color.active.val('hex', localStorage['picker0']);
+            $.jPicker.List[1].color.active.val('hex', localStorage['picker1']);
+
+            // set texts
+            $('#frequency').html(strobo.frequency+' BPM');
+            $('#beat-threshold').html('Threshold: '+strobo.kick_config.threshold);
+            $('#beat-frequency').html('Frequency: '+strobo.kick_config.frequency+' kHz');
+            $('#beat-decay').html('Decay: '+strobo.kick_config.decay);
+        }
+    }
+
+    /*
+        Controls for manual mode
+     */
+
 	// create the slider
 	$('#slider').slider({
 		max: 2000,
 		min: 1,
+        value: strobo.frequency,
 		slide: function(event, ui) {
 					$('#frequency').html(ui.value+' BPM');
 					strobo.frequency = ui.value;
@@ -37,51 +132,61 @@ function setConfig() {
 	$('button', '#sync').button();
 	$('#sync').click(syncStrobo);
 
+    /*
+        Controls for microphone mode
+     */
+
     // beat button
     $('button', '#beat').button();
     $('#beat').click(beatStrobo);
-	
-	// set the two color pickers
-	$('#picker1').jPicker({
-		window:
-		{
-			expandable: true,
-			position:
-			{
-				x: 'right',
-				y: 'bottom'
-			}
-		},
-		color:
-		{
-			active: new $.jPicker.Color({hex: '111111'})
-		},
-		images:
-		{
-			clientPath: 'jpicker/images/'
-		}
 
-	}, modifyStrobo); // don't forget the callback
-	$('#picker2').jPicker({
-		window:
-		{
-			expandable: true,
-			position:
-			{
-				x: 'right',
-				y: 'bottom'
-			}
-		},
-		color:
-		{
-			active: new $.jPicker.Color({hex: 'ffffff'})
-		},
-		images:
-		{
-			clientPath: 'jpicker/images/'
-		}
+    // threshold slider
+    $('#slider-threshold').slider({
+        max: 0.5,
+        min: 0.001,
+        step: 0.001,
+        slide: function(event, ui) {
+            $('#beat-threshold').html('Threshold: '+ui.value);
+            strobo.kick_config.threshold = ui.value;
+            configKick();
+        }
+    });
 
-	}, modifyStrobo);
+    // frequency slider
+    $('#slider-frequency').slider({
+        max: 400,
+        min: 0,
+        step: 1,
+        slide: function(event, ui) {
+            $('#beat-frequency').html('Frequency: '+ui.value+' kHz');
+            strobo.kick_config.frequency = ui.value;
+            configKick();
+        }
+    });
+
+    // threshold slider
+    $('#slider-decay').slider({
+        max: 0.5,
+        min: 0.0001,
+        step: 0.0001,
+        slide: function(event, ui) {
+            $('#beat-decay').html('Decay: '+ui.value);
+            strobo.kick_config.decay = ui.value;
+            configKick();
+        }
+    });
+
+    // reset [0 - 10] link
+    $('#reset-frequency').on('click', function(e) {
+        e.preventDefault();
+        strobo.kick_config.frequency = [0,10];
+        configKick();
+        $('#beat-frequency').html('Frequency: [0,10] kHz');
+    });
+
+    /*
+        Open!
+     */
 	
 	// open it as a dialog
 	$( "#config" ).dialog({
@@ -92,20 +197,22 @@ function setConfig() {
 	});
 
 	// link to the button
-	$('#open').button({icons: {primary: 'ui-icon-extlink'}});
-	$('#open').click(function() {$('#config').dialog('open'); return false;});
+	$('#open')
+        .button({icons: {primary: 'ui-icon-extlink'}})
+	    .click(function() {$('#config').dialog('open'); return false;});
 }
 
 /*
  * Pretty straight-forward functions about the stroboscope
  */
 function startStrobo() {
-	blink = 0;
+	strobo.blink = 0;
 	
 	// set stop button
-	$('#launch').unbind('click'); // destroy previous handler
-	$('#launch').click(stopStrobo);
-	$('#launch').html('<button>Stop animation</button>');
+	$('#launch')
+        .unbind('click')
+	    .click(stopStrobo)
+	    .html('<button>Stop animation</button>');
 	$('button', '#launch').button();
 	
 	launchStrobo();
@@ -137,9 +244,10 @@ function stopStrobo(skipButton) {
 	if (skipButton != true)
 	{
 		// reset start button
-		$('#launch').unbind('click'); // destroy previous handler
-		$('#launch').click(startStrobo);
-		$('#launch').html('<button>Launch animation</button>');
+		$('#launch')
+            .unbind('click') // destroy previous handler
+		    .click(startStrobo)
+		    .html('<button>Launch animation</button>');
 		$('button', '#launch').button();
 	}
 }
@@ -177,19 +285,29 @@ function syncStrobo() {
  * Sync with music BPM
  */
 function beatStrobo() {
-    var
-        dancer = new Dancer(),
-        kick = dancer.createKick({
-            onKick: function (mag) {
-                console.log('kick');
-                //$('body').css('background', '#'+$.jPicker.List[1].color.active.val('hex'));
-            },
-            offKick: function (mag) {
-                console.log('off');
-                //$('body').css('background', '#'+$.jPicker.List[0].color.active.val('hex'));
-            }
-        }).on(); // and kick in!
+    // set stop button
+    $('#beat')
+        .unbind('click')
+        .click(pauseBeat)
+        .html('<button>Stop animation</button>');
+    $('button', '#beat').button();
 
-    dancer.load({microphone: true});
-    dancer.play(); // same bug even without
+    strobo.kick.on();
+    strobo.dancer.load({microphone: true});
+    strobo.running = true;
+    strobo.dancer.play(); // same bug even without
+}
+function configKick() {
+    strobo.kick.set(strobo.kick_config);
+}
+function pauseBeat() {
+    strobo.running = false;
+
+    $('#beat')
+        .unbind('click')
+        .click(beatStrobo)
+        .html('<button>Resume animation</button>');
+    $('button', '#beat').button();
+
+    strobo.kick.off();
 }
